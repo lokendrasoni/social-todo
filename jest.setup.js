5cdef8c91e824bbc35ab342fbeead79e28f8be51
Lokendra Soni <lsoni@dashclicks.com>
2023-03-05 11:02:48 +0530

    Added test data

--- jest.setup.js
+++ jest.setup.js
@@ -5,7 +5,7 @@ module.exports = async function (globalConfig, projectConfig) {
     //dotenv
     require('dotenv').config();
     const mongod = await connect();
-    const loc = path.resolve('./tests/fixtures/db');
+    const loc = path.resolve('./tests/db');
     await migrate(loc);
     globalThis.__MONGOD__ = mongod;
     globalThis.__MONGO_DISCONNECT_CB__ = disconnect;
